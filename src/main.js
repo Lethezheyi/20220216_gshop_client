f9aab82375f62659039a46be115151c9abd22634
Lethezheyi <1328529574@qq.com>
2022-02-16 22:14:26 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 浏览器不显示非生产环境打包的提示
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// 禁用eslint规则
+/* eslint-disable no-unused-vars */
